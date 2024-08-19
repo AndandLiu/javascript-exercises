c8b21673819f273d92c841c306eb0f1c2819a5fa
andrew <andrewliu13531@gmail.com>
2024-08-19 13:03:11 -0700

    My solution to 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removeItems) {
+
+    let newArray = [];
+
+    for(let i = 0; i < array.length; i++) {
+        if(removeItems.indexOf(array[i]) == -1) {
+            newArray.push(array[i]);
+        }
+    }
+
+    return newArray;
 };
 
 // Do not edit below this line
