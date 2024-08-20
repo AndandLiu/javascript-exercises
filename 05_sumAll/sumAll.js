b6e7ba9634900b4c866f38e52b6015809a4d45b6
andrew <andrewliu13531@gmail.com>
2024-08-19 18:31:22 -0700

    My solution to 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    let total = 0;
+    let start,end;
+
+    if(num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+    else if(num1 !== Math.floor(num1) || num2 !== Math.floor(num2)) {
+        return "ERROR"
+    }
+    else if(typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR"
+    }
+
+    if(num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+    else {
+        start = num1;
+        end = num2;
+    }
+
+    for (var i = start; i < end + 1; i++) {
+        total += i;
+    }
+
+    return total
 };
 
 // Do not edit below this line
