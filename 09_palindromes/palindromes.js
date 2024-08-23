7366c3098e97d4596d6a6b7f1928f38705e07451
andrew <andrewliu13531@gmail.com>
2024-08-23 11:36:13 -0700

    My solution to 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let palindromesCheck = true;
 
+    str = str.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"")
+    str = str.replace(/\s/g, '');
+
+    for(let i = 0; i < str.length; i++){
+        if(str[i].toLowerCase() != str[str.length - i - 1].toLowerCase()) {
+            palindromesCheck = false;
+        }
+    }
+    return palindromesCheck;
 };
 
 // Do not edit below this line
