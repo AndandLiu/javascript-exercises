3b0c1f04268da68bcf8939176a27d0648acd1e0c
andrew <andrewliu13531@gmail.com>
2024-08-23 12:00:46 -0700

    My solution to 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    titleArray = [];
 
+    arr.map((obj) => 
+        titleArray.push(obj.title)
+    );
+    
+    return titleArray;
 };
 
 // Do not edit below this line
