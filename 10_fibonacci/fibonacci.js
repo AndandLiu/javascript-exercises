f9df4c5c57affa1c90c9ae5f118b2979f436a732
andrew <andrewliu13531@gmail.com>
2024-08-23 11:46:06 -0700

    My solution to 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let prevNum = 0;
+    let currentNum = 1;
 
+    if(num == 0){
+        return 0;
+    }
+    else if(num < 0){
+        return "OOPS";
+    }
+
+    for(let i = 1; i < parseInt(num); i++){
+        const tempNum = currentNum;
+        currentNum = currentNum + prevNum;
+        prevNum = tempNum
+    }
+
+    return currentNum;
 };
 
 // Do not edit below this line
