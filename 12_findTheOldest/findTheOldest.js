d0cc9df481dbd40eb1d430913c610416288afb8d
andrew <andrewliu13531@gmail.com>
2024-08-23 12:43:15 -0700

    My solution to 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,26 @@
-const findTheOldest = function() {
+const findTheOldest = function(array) {
+    return array.reduce((oldest, currentItem) => {
+        let oldestDeath = oldest.yearOfDeath;
+        let currentDeath = currentItem.yearOfDeath;
 
+        if(oldest.yearOfDeath == undefined) {
+            oldestDeath = new Date().getFullYear()
+        }
+        const oldestAge = oldestDeath - oldest.yearOfBirth;
+        
+
+        if(currentItem.yearOfDeath == undefined) {
+            currentDeath = new Date().getFullYear()
+        }
+        const currentItemAge = currentDeath - currentItem.yearOfBirth;
+        
+
+        if(oldestAge < currentItemAge) {
+            oldest = currentItem;
+        }
+        
+        return oldest;
+    })
 };
 
 // Do not edit below this line
